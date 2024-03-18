94f5177f8b95467512ee9d2a870382a6df48aa5f
khushboo <khushboodesai2609@gmail.com>
2024-03-18 21:19:38 +0530

    comit

--- src/components/dashboard/ExplorAsSeller/SellerReg.jsx
+++ src/components/dashboard/ExplorAsSeller/SellerReg.jsx
@@ -48,7 +48,7 @@ function SellerReg() {
         <form onSubmit={handleSubmit} ref={form}>
             <div className="mb-3">
                 <label htmlFor="categories" className="form-label">Category</label>
-                <select  className="form-select" id="categories" name="category" multiple>
+                <select  className="form-select" id="categories" name="categories" multiple>
                         {
                              category.map((data, id) => <option key={id} value={data}>{data}</option>)
                         }
@@ -58,10 +58,7 @@ function SellerReg() {
                 <label htmlFor="description" className="form-label">Description</label>
                 <textarea className="form-control" id="description" name="description" rows="3"></textarea>
             </div>
-            <div className="mb-3">
-                <label htmlFor="location" className="form-label">Location</label>
-                <input type="text" className="form-control" id="location" name="location" />
-            </div>
+          
             <br></br>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
