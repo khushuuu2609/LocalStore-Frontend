23a65ab7c20636c06abfb6d87be3dada94946160
khushboo <khushboodesai2609@gmail.com>
2024-04-03 16:19:56 +0530

    footer

--- src/components/dashboard/Footer.jsx
+++ src/components/dashboard/Footer.jsx
@@ -1,55 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
     return (
         <>
-            <div className="container-fluid bg-dark text-white-50 footer pt-5">
-                <div className="container py-5">
-                    <div className="row g-5">
-                        <div className="col-lg-3 col-md-6">
-                            <div className="footer-item">
-                                <h4 className="text-light mb-3">Why People Like us!</h4>
-                                <p className="mb-4"><b>Transforming the buying and selling journey :</b> Upload a photo, find your match seller.
-                                Experience effortless transactions at every step. 
-                                Connect with buyers and sellers seamlessly through our notification system.</p>
+            <div className="container-fluid bg-dark text-white-50 footer">
+                <div className="container">
+                    <div className="row justify-content-center g-5">
+                            <div className="footer-item text-center">
+                                <h6 className="text-light mb-3">Why People Like us!</h6>
+                                <p className="mb-4">
+                                    <b>Transforming the buying and selling journey :</b> Upload a photo, find your match seller.
+                                    Connect with buyers and sellers seamlessly through our notification system.<br/>
+                                    <b>Contact Us</b> - 8401634399  <b>Email</b> - Demo@gmail.com
+                                </p>
                             </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="d-flex flex-column text-start footer-item">
-                                <h4 className="text-light mb-3">Shop Info</h4>
-                                <a className="btn-link" href="">About Us</a>
-                                <a className="btn-link" href="">Contact Us</a>
-                                <a className="btn-link" href="">Privacy Policy</a>
-                                <a className="btn-link" href="">Terms & Condition</a>
-                                <a className="btn-link" href="">Return Policy</a>
-                                <a className="btn-link" href="">FAQs & Help</a>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="d-flex flex-column text-start footer-item">
-                                <h4 className="text-light mb-3">Account</h4>
-                                <a className="btn-link" href="">My Account</a>
-                                <a className="btn-link" href="">Shop</a>
-                                <a className="btn-link" href="">Shopping Cart</a>
-                                <a className="btn-link" href="">Orders</a>
-                                <a className="btn-link" href="">Order History</a>
-                                <a className="btn-link" href="">Track Order</a>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-6">
-                            <div className="footer-item">
-                                <h4 className="text-light mb-3">Contact</h4>
-                                <p>Email: Example@gmail.com</p>
-                                <p>Phone: +0123 4567 8910</p>
-                                <p>Payment Accepted</p>
-                                <img src="img/payment.png" className="img-fluid" alt="" />
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
